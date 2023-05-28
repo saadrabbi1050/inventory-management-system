ef6a3a1f37a6962d2c7d61b498d12531a2eed2b4
saadrabbi1050 <saadrabbi9@gmail.com.com>
2023-05-29 00:28:27 +0600

    store view

--- resources/views/backend/stores/store-view.blade.php
+++ resources/views/backend/stores/store-view.blade.php
@@ -4,11 +4,11 @@
 @include('backend.layouts.includes.message')
     <div class="container">
         <div class="card">
-            <div class="card-header">Create Store</div>
-            <div class="card-body">
+            <div class="card-header bg-secondary text-white fs-3 text-center">Store View</div>
+            <div class="card-body mb-2 bg-secondary text-black">
                 <!-- Horizontal Form -->
                     @foreach($stores as $store) 
-                        <div class="card p-3">
+                        <div class="card p-3 mt-4 bg-info text-dark">
                             {{ $store->name }}  
                                 @foreach($store->racks as $rack)
 
@@ -18,8 +18,8 @@
 
                                             <div class="row">
                                                 @foreach($rack->boxes as $box)
-                                                    <div class="col-md-2">
-                                                        <div style="border:1px solid red ; height:40px; width:80px; margin:5px">
+                                                    <div class="col-md-2 text-center ">
+                                                        <div style="border:2px solid red ; height:40px; width:80px; margin:5px;">
                                                             {{ $box->name }}
                                                         </div>
                                                     </div>
